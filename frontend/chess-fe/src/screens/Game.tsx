ca14df4b89da8c9b3a417c02381acb364983f510
Vedant-05 <vedantj76@gmail.com>
2024-05-30 18:37:34 +0530

    Initial commit

---
+++ frontend/chess-fe/src/screens/Game.tsx
@@ -0,0 +1,9 @@
+export const Game = () => {
+  return (
+    <div className="justify-center flex">
+      <div className="pt-8 max-w-screen-lg">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2"></div>
+      </div>
+    </div>
+  );
+};
